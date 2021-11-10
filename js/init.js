2156fda7b2636cf5fa0a1dca835f01b5e3b0d519
yyyaaan <yan@yan.fi>
2021-11-10 10:43:07 +0000

    added JS expand/collapse all functionalities

--- js/init.js
+++ js/init.js
@@ -1,7 +1,10 @@
 $(document).ready(function(){
+  $("#btn-a2").fadeOut();
+  $("#btn-b2").fadeOut();
   $('.tooltipped').tooltip();
   $('.sidenav').sidenav();
-  $('.collapsible').collapsible();
+  $('.tabs').tabs();
+  $('.collapsible').collapsible({ accordion: false });
   $('.materialboxed').materialbox({
     onOpenStart: function(e){
       e.src = e.attributes['data-enlarge-image'].nodeValue;
@@ -9,9 +12,30 @@ $(document).ready(function(){
   });
 
   M.toast({html: '<- More details here!'})
-  
-  var el = document.querySelector('.tabs');
-  var instance = M.Tabs.init(el, {});
 });
+
+function expandAllAcademics() {
+  $("#btn-a2").fadeIn();  $("#btn-a1").fadeOut();
+  var instance = M.Collapsible.getInstance($("#cert-academic > .collapsible"));
+  for (var i = 1; i < instance.$el[0].children.length; i++) instance.open(i);
+}
+
+function collapseAllAcademics() {
+  $("#btn-a1").fadeIn();  $("#btn-a2").fadeOut();
+  var instance = M.Collapsible.getInstance($("#cert-academic > .collapsible"));
+  for (var i = 1; i < instance.$el[0].children.length; i++) instance.close(i);
+}
+
+function expandAllSpecializations() {
+  $("#btn-b2").fadeIn();  $("#btn-b1").fadeOut();
+  var instance = M.Collapsible.getInstance($("#cert-specializations > .collapsible"));
+  for (var i = 1; i < instance.$el[0].children.length; i++) instance.open(i);
+}
+
+function collapseAllSpecializations() {
+  $("#btn-b1").fadeIn();  $("#btn-b2").fadeOut();
+  var instance = M.Collapsible.getInstance($("#cert-specializations > .collapsible"));
+  for (var i = 1; i < instance.$el[0].children.length; i++) instance.close(i);
+}
 
 
